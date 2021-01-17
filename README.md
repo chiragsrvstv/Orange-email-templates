7df844aab704cca5d857133c537bff8b885bb506
chiragsrvstv <chirag.srivastava@live.com>
2021-01-17 23:28:34 +0530

    🎉 Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Orange Email Templates
+Ever thought of customizing a email templates. With Orange-Email-Templates you can generate and customize cool html email templates and can easily use them to your projects.
+
+## Installation
+1. Download or clone the repository.
+2. Make sure to have Node and npm installed.
+3. Create a `.env` file and add `PORT:**your-port**` to it.
+3. Run `npm start` to run the local server at your desired port.
+
+
---
+++ src/generator.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded());
+
+const port = process.env.PORT;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.listen(port, () => {
+  console.log(`Listening at PORT:${port}`);
+});
+
+app.get('/', (req, res) => res.redirect('/template'));
+
+app.get('/template', (req, res) => res.render('home.ejs'));
+
+app.post('/template', (req, res) => {
+  res.header('Content-Type', 'text/plain');
+
+  const {
+    company, signature, title, style,
+  } = req.body;
+  if (style === 'simple') {
+    return res.render('simpletemplate.ejs', { signature, company, title });
+  }
+  return res.render('template.ejs', { signature, company, title });
+});
+
+app.get('*', (req, res) => res.send('<h1> 404: You seem to be lost <h1>'));
